7390382a52c3d500d12f1bb2d2b4f80c70ece3ad
heyony312 <hyeony312@postech.ac.kr>
2024-09-19 10:08:23 +0000

    Initial commit

---
+++ .bash_history
@@ -0,0 +1,84 @@
+gcc --version
+sudo apt install gcc
+ls
+gcc --version
+uname -a
+pwd
+git clone git://pintos-os.org/pintos-anon pintos
+chmod +x setup.sh 
+./setup.sh 
+echo $PINTOSPATH
+./setup.sh 
+cd pintos/src/threads/
+make
+sudo apt install make
+make
+cd build/
+make check
+cd ../../
+ls
+cd utils
+ls
+sudo apt install qemu-system-i386
+cd ../
+cd threads/
+make clean
+make 
+cd build/
+make check
+echo $PINTOSPATH
+exit
+date
+ls
+cd pintos
+dir
+ls
+cd src
+ls
+cd tests
+ls
+cd threads
+ls
+cd Make
+make
+cd ..
+cd
+git init
+ls
+cd home
+ls
+cd
+ls
+cd .git
+cd
+rm .git
+rmdir .git
+del .git
+rmd .git
+rmdir .git
+ls
+cd .git
+cd
+cd .git
+ls
+cd
+cd pintos
+git init
+cd
+cd .git
+cd
+git remote add origin https://github.com/hyeony312/CSED312.git
+git add .
+git status
+git commit
+push
+git push
+git branch -M main
+ls
+cd pintos
+ls
+cd src
+ls
+cd threads
+ls
+cat thread.c
---
+++ .vscode-server/data/User/History/27c0b27d/rzSI.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Step 1: Add environment variables to ~/.bashrc
+echo 'export PINTOSPATH="/home/user/pintos"' >> ~/.bashrc
+echo 'export PATH=$PINTOSPATH/src/utils:$PATH' >> ~/.bashrc
+
+source ~/.bashrc
+
+# Step 2: Replace line 7 of pintos/src/threads/Make.vars
+sed -i '7s/SIMULATOR = --bochs/SIMULATOR = --qemu/' "$PINTOSPATH/src/threads/Make.vars"
+
+# Step 3: Replace line 5 of pintos/src/utils/Makefile
+sed -i '5s/LOADLIBES = -lm/LDLIBS = -lm/' "$PINTOSPATH/src/utils/Makefile"
+
+# Step 4: Replace line 104 of pintos/src/utils/pintos
+sed -i '104s/\$sim = "bochs" if !defined \$sim;/\$sim = "qemu" if !defined \$sim;/' "$PINTOSPATH/src/utils/pintos"
+
+# Step 5: Replace line 4 of pintos/src/utils/pintos-gdb
+sed -i '4s|GDBMACROS=/usr/class/cs140/pintos/pintos/src/misc/gdb-macros|GDBMACROS=$PINTOSPATH/src/misc/gdb-macros|' "$PINTOSPATH/src/utils/pintos-gdb"
+
+# Reload shell configuration
+source ~/.bashrc
+
+echo "All steps have been completed successfully."
